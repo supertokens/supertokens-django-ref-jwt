275db617251e2e18aa084cdee33e2901e1643ad3
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2019-08-08 13:35:06 +0530

    cookie.py updated

--- supertokens_session/cookie.py
+++ supertokens_session/cookie.py
@@ -4,6 +4,10 @@ from .constant import (
     ID_REFRESH_TOKEN_COOKIE_KEY,
     ANTI_CSRF_HEADER_KEY
 )
+from .exceptions import raise_general_exception
+from datetime import datetime
+from .settings import supertokens_settings
+
 def set_options_api(response):
     set_header(response, 'ACCESS-CONTROL-ALLOW-HEADERS', ANTI_CSRF_HEADER_KEY)
     set_header(response, 'ACCESS-CONTROL-ALLOW-CREDENTIALS', 'true')
@@ -26,3 +30,36 @@ def get_cookie(request, key):
 
 def set_cookie(response, key, value, expires, path, domain, secure, httponly):
     response.set_cookie(key=key, value=value, expires=expires, path=path, domain=domain, secure=secure, httponly=httponly)
+
+def attach_anti_csrf_header_if_required(response, value):
+    if supertokens_settings.ANTI_CSRF_ENABLE:
+        if value == None:
+            raise_general_exception('BUG: anti-csrf token is null. if you are getting this error, please report this as a bug')
+        set_header(response, ANTI_CSRF_HEADER_KEY, value)
+        set_header(response, 'ACCESS-CONTROL-EXPOSE-HEADERS', ANTI_CSRF_HEADER_KEY)
+
+def get_anti_csrf_header(request):
+    get_header(request, ANTI_CSRF_HEADER_KEY)
+
+def clear_session_from_cookie(response):
+    set_cookie(response, ACCESS_TOKEN_COOKIE_KEY, '',  datetime.fromtimestamp(0), supertokens_settings.ACCESS_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, supertokens_settings.COOKIE_SECURE, True)
+    set_cookie(response, ID_REFRESH_TOKEN_COOKIE_KEY, '',  datetime.fromtimestamp(0), supertokens_settings.ACCESS_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, False, False)
+    set_cookie(response, REFRESH_TOKEN_COOKIE_KEY, '',  datetime.fromtimestamp(0), supertokens_settings.REFRESH_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, supertokens_settings.COOKIE_SECURE, True)
+
+def attach_access_token_to_cookie(response, token, expires_at):
+    set_cookie(response, ACCESS_TOKEN_COOKIE_KEY, token,  expires_at, supertokens_settings.ACCESS_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, supertokens_settings.COOKIE_SECURE, True)
+
+def attach_refresh_token_to_cookie(response, token, expires_at):
+    set_cookie(response, REFRESH_TOKEN_COOKIE_KEY, token,  expires_at, supertokens_settings.REFRESH_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, supertokens_settings.COOKIE_SECURE, True)
+
+def attach_id_refresh_token_to_cookie(response, token, expires_at):
+    set_cookie(response, ID_REFRESH_TOKEN_COOKIE_KEY, token,  expires_at, supertokens_settings.ACCESS_TOKEN_PATH, supertokens_settings.COOKIE_DOMAIN, False, False)
+
+def get_access_token_from_cookie(request):
+    return get_cookie(request, ACCESS_TOKEN_COOKIE_KEY)
+
+def get_refresh_token_from_cookie(request):
+    return get_cookie(request, REFRESH_TOKEN_COOKIE_KEY)
+
+def get_id_refresh_token_from_cookie(request):
+    return get_cookie(request, ID_REFRESH_TOKEN_COOKIE_KEY)
