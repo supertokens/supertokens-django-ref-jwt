086c58c820cc35ac17095bca842709f26908f8d0
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-08-22 17:57:55 +0530

    version change to test again [ci skip]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require = {
 
 setup(
     name="supertokens_jwt_ref",
-    version="0.0.5",
+    version="0.0.6",
     author="Bhumil Sarvaiya, Rishabh Poddar",
     license="MIT",
     author_email="sarvaiyabhumil@gmail.com, rishabh@supertokens.io",
