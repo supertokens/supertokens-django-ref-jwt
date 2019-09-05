0105ef1aab63ae05acb4d78d7c89537fb035c664
Nemi Shah <nemishah1212@gmail.com>
2019-09-05 17:02:30 +0530

    Add latest version to feedback api

--- website/static/scripts/utils.js
+++ website/static/scripts/utils.js
@@ -1,3 +1,9 @@
+let latestVersion = "1.0.X"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
+
 function getGithubUrl() {
     return "https://github.com/supertokens/supertokens-django-ref-jwt";
+}
+
+function getLatestVersion() {
+    return latestVersion;
 }
